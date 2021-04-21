a3398b70cb8304e23acffc245afd0736e7773ab8
ArchitShrikar <82526566+ArchitShrikar@users.noreply.github.com>
2021-04-22 01:28:45 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,39 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+a=2
+b=3
 @app.route("/")
 def index():
-    return "Hello, world!"
+    return a+b 
+@app.route("/sub")
+def sub():
+    return a-b
+@app.route("/mul")
+def mul():
+    return a*b
+@app.route("/power")
+def power():
+    return a**b
+@app.route("/exp")
+def exp():
+    return a*b+a**b
+
+#"Hello, world!"
 
 
 if __name__ == "__main__":
     app.run()
+
+#from flask import Flask
+
+#app = Flask(__name__)
+
+#@app.route("/")
+#def index():
+#    return "Hello, world!"
+
+
+#if __name__ == "__main__":
+#    app.run()
 
