e195cda2c325b053001b1fab582630fb7a8cd130
anmol855 <71627362+anmol855@users.noreply.github.com>
2021-04-22 23:23:20 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,7 +5,7 @@ from app import power
 from app import exp
 
 def test_index():
-    assert index() == '5'
+    assert index() == '"the sum of",str(2),"and",str(3),"is",str(5)'
 def test_sub():
     assert sub() == '-1'
 def test_mul():
